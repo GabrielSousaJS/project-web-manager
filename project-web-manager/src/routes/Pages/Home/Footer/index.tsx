237391ae9a518c398860cf72bdfcfea95008e4d7
Gabriel Santos <gabriels.santosx@gmail.com>
2024-08-28 20:14:22 -0300

    fix: Localização Google Maps

--- project-web-manager/src/routes/Pages/Home/Footer/index.tsx
+++ project-web-manager/src/routes/Pages/Home/Footer/index.tsx
@@ -8,24 +8,16 @@ export function Footer() {
           <img src={logo} alt='Logo RSX Capital' />
         </div>
         <div className='text-sm'>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem veniam
-          recusandae, explicabo minus odit tenetur dolorem deleniti, impedit facere
-          nam et laborum provident perspiciatis commodi molestiae nesciunt at est
-          praesentium. Vitae, explicabo officiis. Ea sequi alias at perferendis
-          voluptatum, maxime veniam delectus facilis labore quaerat!
-        </div>
-      </div>
-      <div className='flex-1 pb-6 md:pb-0'>
-        <h4 className='font-semibold pb-3'>ONDE ESTAMOS</h4>
-        <div className='text-sm'>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro,
-          quaerat provident natus temporibus error doloribus.
+        Além disso, valorizamos a educação financeira como um pilar essencial
+        para o sucesso de nossos investidores. Oferecemos recursos e suporte para
+        que você possa tomar decisões informadas e construir um futuro financeiro
+        sólido.
         </div>
       </div>
       <div className='flex-1'>
         <iframe
           title='Localização RSX Capital'
-          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d914.1508387133016!2d-46.624409536345446!3d-23.582685027140798!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce5b6a57fa2403%3A0x1c8dca4543e75da7!2sBuras!5e0!3m2!1spt-BR!2sbr!4v1721207620038!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade'
+          src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d604.4141426963555!2d-47.935434046044016!3d-15.867597369174598!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x935a2f9df671b1ab%3A0x222412246d541c11!2sEPAR%2C%202915%20-%20Bras%C3%ADlia%2C%20DF%2C%2070297-400!5e0!3m2!1spt-BR!2sbr!4v1724886689750!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade'
           loading='lazy'
           className='w-full h-full' />
       </div>
